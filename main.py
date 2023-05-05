46aa180ad092979d02fd55478816e6b4161f833e
Hanna <88632861+taimuti@users.noreply.github.com>
2023-05-05 19:20:12 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import sys
+import os
+import heapq
+
 # Словарь для хранения списка ребер графа
 graph = {}
+# Словарь для хранения видов транспорта
+transport = []
+# Словарь для хранения всех городов
+cities = []
 
 # Функция для добавления ребер в граф
 def add_edge(from_city, to_city, transport_type, cruise_time, cruise_fare):
@@ -10,7 +18,7 @@ def add_edge(from_city, to_city, transport_type, cruise_time, cruise_fare):
         graph[to_city] = {}
     # Добавляем ребро от города отправления к городу прибытия
     graph[from_city][to_city] = (transport_type, cruise_time, cruise_fare)
-
+    
 # Чтение графа из файла
 def read_graph(filename):
     with open(filename, 'r') as f:
@@ -20,6 +28,9 @@ def read_graph(filename):
                 continue
             from_city, to_city, transport_type, cruise_time, cruise_fare = line.strip().split()
             add_edge(from_city.strip('"'), to_city.strip('"'), transport_type.strip('"'), int (cruise_time), int(cruise_fare))
+            if transport_type.strip('"') not in transport: transport.append(transport_type.strip('"'))
+            if from_city.strip('"') not in cities: cities.append(from_city.strip('"'))
+            if to_city.strip('"') not in cities: cities.append(to_city.strip('"'))
 
 def dijkstra_shortest_time(from_city, to_city, allowed_transport_types):
     # Создаем словарь для хранения кратчайшего времени проезда от начального города до остальных городов
@@ -31,8 +42,7 @@ def dijkstra_shortest_time(from_city, to_city, allowed_transport_types):
     # Пока есть необработанные города
     while unprocessed_cities:
         # Получаем город с минимальным временем проезда
-        current_time, current_city = min(unprocessed_cities)
-        unprocessed_cities.remove((current_time, current_city))
+        (current_time, current_city) = heapq.heappop(unprocessed_cities)
         # Если достигли целевого города, возвращаем путь и время проезда
         if current_city == to_city:
             return (path[current_city], shortest_time[current_city])
@@ -49,10 +59,7 @@ def dijkstra_shortest_time(from_city, to_city, allowed_transport_types):
                 shortest_time[neighbor_city] = new_time
                 path[neighbor_city] = path[current_city] + [neighbor_city]
                 # Добавляем соседний город в список необработанных городов
-                i = 0
-                while i < len(unprocessed_cities) and unprocessed_cities[i][0] <= new_time:
-                    i += 1
-                unprocessed_cities.insert(i, (new_time, neighbor_city))
+                heapq.heappush(unprocessed_cities, (new_time, neighbor_city))
     # Если целевой город недостижим из начального, возвращаем None
     return None
 
@@ -66,8 +73,7 @@ def dijkstra_lowest_cost(from_city, to_city, allowed_transport_types):
     # Пока есть необработанные города
     while unprocessed_cities:
         # Получаем город с минимальной стоимостью проезда
-        current_cost, current_city = min(unprocessed_cities)
-        unprocessed_cities.remove((current_cost, current_city))
+        (current_cost, current_city) = heapq.heappop(unprocessed_cities)
         # Если достигли целевого города, возвращаем путь и стоимость проезда
         if current_city == to_city:
             return (path[current_city], lowest_cost[current_city])
@@ -84,10 +90,7 @@ def dijkstra_lowest_cost(from_city, to_city, allowed_transport_types):
                 lowest_cost[neighbor_city] = new_cost
                 path[neighbor_city] = path[current_city] + [neighbor_city]
                 # Добавляем соседний город в список необработанных городов
-                i = 0
-                while i < len(unprocessed_cities) and unprocessed_cities[i][0] <= new_cost:
-                    i += 1
-                unprocessed_cities.insert(i, (new_cost, neighbor_city))
+                heapq.heappush(unprocessed_cities, (new_cost, neighbor_city))
     # Если целевой город недостижим из начального, возвращаем None
     return None
 
@@ -107,7 +110,7 @@ def bfs(from_city, to_city, allowed_transport_types):
                 current_city = visited[current_city]
             return path[::-1]
         # Просматриваем соседей текущего города
-        for neighbor_city, transport in graph[current_city].items():
+        for neighbor_city, transport in graph[current_city].items(): 
             transport_type, cruise_time, cruise_fare = transport
             # Проверяем, что разрешенный вид транспорта встречается в текущем ребре
             if transport_type in allowed_transport_types and neighbor_city not in visited:
@@ -117,51 +120,75 @@ def bfs(from_city, to_city, allowed_transport_types):
     return None
 
 def dijkstra_limited_cost(from_city, allowed_transport_types, limit_cost):
-    # Создаем словарь для хранения кратчайших путей
-    shortest_paths = {from_city: (None, 0)}
-    # Словарь для хранения путей
-    path = {}
-    # Список для хранения непроверенных городов
-    unvisited_cities = [from_city]
-    while unvisited_cities:
-        # Выбираем город с наименьшей стоимостью
-        current_city = min([(shortest_paths[c][1], c) for c in unvisited_cities])[1]
-        # Проходим по всем городам, соседним с текущим
-        for neighbor_city in graph[current_city]:
-            # Проверяем, разрешено ли использование транспорта, которым можно добраться до города-соседа
-            if graph[current_city][neighbor_city][0] in allowed_transport_types:
-                # Вычисляем стоимость пути от текущего города до города-соседа
-                cost = shortest_paths[current_city][1] + graph[current_city][neighbor_city][2]
-                # Проверяем, не превышает ли стоимость ограничение limit_cost
-                if limit_cost is None or cost <= limit_cost:
-                    # Если это лучший путь до города-соседа или если путь еще не найден
-                    if neighbor_city not in shortest_paths or cost < shortest_paths[neighbor_city][1]:
-                        # Обновляем стоимость пути до города-соседа
-                        shortest_paths[neighbor_city] = (current_city, cost)
-                        # Обновляем путь до города-соседа
-                        path[neighbor_city] = graph[current_city][neighbor_city][0]
-                    # Добавляем город-соседа в список непроверенных городов
-                    if neighbor_city not in unvisited_cities:
-                        unvisited_cities.append(neighbor_city)
-        # Удаляем текущий город из списка непроверенных городов
-        unvisited_cities.remove(current_city)
-    # Формируем результат
-    result = {}
-    for city in shortest_paths:
-        if shortest_paths[city][0] is not None:
-            result[city] = {'cost': shortest_paths[city][1], 'path': []}
-            # Добавляем города в путь от текущего до стартового города
-            while city is not None:
-                result[city]['path'].append(path.get(city))
-                city = shortest_paths[city][0]
-            # Разворачиваем путь в правильном порядке и удаляем первый элемент (стартовый город)
-            result[city]['path'] = result[city]['path'][::-1][1:]
-    return result
-    
-# Читаем граф из файла
-read_graph("input.txt")
+    # Словарь для хранения кратчайших расстояний до каждого города
+    distances = {city: float('inf') for city in cities}
+    distances[from_city] = 0
+    # Запоминание путей
+    path ={}
+    # Очередь с приоритетами для хранения пар (расстояние до города, город)
+    queue = [(0, from_city)]
+    # Цикл, пока очередь с приоритетами не пуста
+    while queue:
+        # Извлекаем из очереди город с минимальной стоимостью
+        curr_cost, curr_city = heapq.heappop(queue)
+        if (curr_cost > distances[curr_city]): 
+            continue
+        # Если стоимость до текущего города больше, чем ограничение, то выходим из цикла
+        if curr_cost > limit_cost: 
+            break
+        # Обходим всех соседей текущего города
+        for neighbor, (transport_type, cruise_time, cruise_fare) in graph[curr_city].items():
+            # Если тип транспорта не входит в список разрешенных, то пропускаем город
+            if transport_type not in allowed_transport_types: 
+                continue
+            # Вычисляем новую стоимость до соседа через текущий город
+            new_cost = distances[curr_city] + cruise_fare
+            if ((distances[neighbor] > new_cost) and (new_cost <= limit_cost)):
+                distances[neighbor] = new_cost
+                path[neighbor] = [curr_city, neighbor, transport_type, cruise_fare]
+                heapq.heappush(queue,[distances[neighbor], neighbor])
+    # Выводим кратчайшие пути до всех городов, до которых можно добраться за ограниченную стоимость
+    for (to_city, [from_city, to_city, transport_type, cruise_fare]) in path.items():
+        print(f'{to_city} : {from_city} -> {to_city}, {transport_type}, {cruise_fare}')
 
-print("Режимы работы программы:\n 1. Среди кратчайших по времени путей между двумя городами найти путь минимальной стоимости.\n 2. Среди минимальных по стоимочти путей между двумя городами найти кратчайший по времени путь.\n 3. Найти путь между двумя городами, минимальный по числу посещенных городов.\n")
+def dijkstra_limited_time(from_city, allowed_transport_types, limit_time):
+    # Словарь для хранения минимального времени до каждого города
+    distances = {city: float('inf') for city in cities}
+    distances[from_city] = 0
+    # Запоминание путей
+    path ={}
+    # Очередь с приоритетами для хранения пар (время до города, город)
+    queue = [(0, from_city)]
+    # Цикл, пока очередь с приоритетами не пуста
+    while queue:
+        # Извлекаем из очереди город с минимальной стоимостью
+        curr_time, curr_city = heapq.heappop(queue)
+        if (curr_time > distances[curr_city]): 
+            continue
+        # Если стоимость до текущего города больше, чем ограничение, то выходим из цикла
+        if curr_time > limit_time: 
+            break
+        # Обходим всех соседей текущего города
+        for neighbor, (transport_type, cruise_time, cruise_fare) in graph[curr_city].items():
+            # Если тип транспорта не входит в список разрешенных, то пропускаем город
+            if transport_type not in allowed_transport_types: 
+                continue
+            # Вычисляем новую стоимость до соседа через текущий город
+            new_time = distances[curr_city] + cruise_time
+            if ((distances[neighbor] > new_time) and (new_time <= limit_time)):
+                distances[neighbor] = new_time
+                path[neighbor] = [curr_city, neighbor, transport_type, cruise_time]
+                heapq.heappush(queue,[distances[neighbor], neighbor])
+    # Выводим кратчайшие пути до всех городов, до которых можно добраться за ограниченную стоимость
+    for (to_city, [from_city, to_city, transport_type, cruise_time]) in path.items():
+        print(f'{to_city} : {from_city} -> {to_city}, {transport_type}, {cruise_time}')
+
+# Читаем граф из файла
+# read_graph("cubes.txt")
+filename = os.getcwd() + '/' + sys.argv[1]
+read_graph(filename)
+
+print("Режимы работы программы:\n 1. Среди кратчайших по времени путей между двумя городами найти путь минимальной стоимости.\n 2. Среди минимальных по стоимоcти путей между двумя городами найти кратчайший по времени путь.\n 3. Найти путь между двумя городами, минимальный по числу посещенных городов.\n")
 print("Выберите режим работы программы, введите цифру")
 mode = input()
 if mode=="1":
@@ -169,12 +196,12 @@ if mode=="1":
     city_from = input()
     print("Введите город прибытия: ")
     city_to = input()
-    print("Введите разрешенные виды транспорта через пробел (все / автобус, поезд, самолёт): ")
+    print("Введите разрешенные виды транспорта через пробел, если хотите разрешить все виды, введите ""все"":")
     input_transport = input().split()
-    if input_transport == "все":
-        allowed_transport = ['автобус', 'поезд', 'самолёт']
+    if input_transport == ["все"]:
+        allowed_transport = transport
     else: allowed_transport = input_transport
-
+ 
     result = dijkstra_shortest_time(city_from, city_to, allowed_transport)
     if result is None:
         print("Целевой город недостижим при заданных параметрах")
@@ -182,15 +209,16 @@ if mode=="1":
         path, time = result
         print("Путь:", " -> ".join(path))
         print("Время: ", time)
+        
 elif mode=="2": 
     print("Введите город отправления: ")
     city_from = input()
     print("Введите город прибытия: ")
     city_to = input()
-    print("Введите разрешенные виды транспорта через пробел (все / автобус, поезд, самолёт): ")
+    print("Введите разрешенные виды транспорта через пробел, если хотите разрешить все виды, введите ""все"":")
     input_transport = input().split()
-    if input_transport == "все":
-        allowed_transport = ['автобус', 'поезд', 'самолёт']
+    if input_transport == ["все"]:
+        allowed_transport = transport
     else: allowed_transport = input_transport
 
     result = dijkstra_lowest_cost(city_from, city_to, allowed_transport)
@@ -200,19 +228,68 @@ elif mode=="2":
         path, cost = result
         print("Путь:", " -> ".join(path))
         print("Стоимость: ", cost)
+
 elif mode=="3":
     print("Введите город отправления: ")
     city_from = input()
     print("Введите город прибытия: ")
     city_to = input()
-    print("Введите разрешенные виды транспорта через пробел (все / автобус, поезд, самолёт): ")
+    print("Введите разрешенные виды транспорта через пробел, если хотите разрешить все виды, введите ""все"":")
     input_transport = input().split()
     if input_transport == ["все"]:
-        allowed_transport = ['автобус', 'поезд', 'самолёт']
+        allowed_transport = transport
     else: allowed_transport = input_transport
-    print(allowed_transport)
+
     result = bfs(city_from, city_to, allowed_transport)
     if result is None:
         print("Целевой город недостижим при заданных параметрах")
     else:
         print("Путь:", " -> ".join(result))
+
+elif mode=="4":
+    print("Введите город отправления: ")
+    city_from = input()
+    print("Введите ограничение по стоимости: ")
+    cost_limit = input()
+    print("Введите разрешенные виды транспорта через пробел, если хотите разрешить все виды, введите ""все"":")
+    input_transport = input().split()
+    if input_transport == ["все"]:
+        allowed_transport = transport
+    else: allowed_transport = input_transport
+
+    dijkstra_limited_cost(city_from, allowed_transport, cost_limit)
+
+elif mode=="5":
+    print("Введите город отправления: ")
+    city_from = input()
+    print("Введите ограничение по времени: ")
+    time_limit = input()
+    print("Введите разрешенные виды транспорта через пробел, если хотите разрешить все виды, введите ""все"":")
+    input_transport = input().split()
+    if input_transport == ["все"]:
+        allowed_transport = transport
+    else: allowed_transport = input_transport
+
+    dijkstra_limited_time(city_from, allowed_transport, time_limit)
+
+# result_3 = bfs("Москва", "Новгород", ["поезд", "автобус", "самолёт"])
+# if result_3 is None:
+#     print("Целевой город недостижим при заданных параметрах")
+# else:
+#     print("Путь:", " -> ".join(result_3))
+
+# result_1 = dijkstra_shortest_time("Севастополь", "Бологое", ["самолёт", "автобус", "поезд"])
+# if result_1 is None:
+#     print("Целевой город недостижим при заданных параметрах")
+# else:
+#     path, time = result_1
+#     print("Путь:", " -> ".join(path))
+#     print("Время: ", time)
+
+# result_2 = dijkstra_lowest_cost("Москва", "Новгород", ["поезд", "автобус", "самолёт"])
+# if result_2 is None:
+#     print("Целевой город недостижим при заданных параметрах")
+# else:
+#     path, cost = result_2
+#     print("Путь:", " -> ".join(path))
+#     print("Стоимость: ", cost)
